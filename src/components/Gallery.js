698d59e876a97b1eb65b624a49cde412c742f535
Kaden Hansen <kadendeanhansen@gmail.com>
2022-03-19 11:16:43 -0400

    rendered starting components to page

---
+++ src/components/Gallery.js
@@ -0,0 +1,10 @@
+import React from "react";
+import GalleryItem from "./GalleryItem";
+
+export default function Gallery () {
+    return (
+        <div>
+            <GalleryItem />
+        </div>
+    )
+}
