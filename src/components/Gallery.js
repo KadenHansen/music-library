cde0e7372fb925abec76658bfb95d943f9bc248d
Kaden Hansen <kadendeanhansen@gmail.com>
2022-03-20 16:08:53 -0400

    refactor gallery to work with context

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { useContext } from "react";
+import { DataContext } from "../context/DataContext";
 import GalleryItem from "./GalleryItem";
 
 export default function Gallery () {
+    const data = useContext(DataContext)
+
+    const dataDisplay = data.map((item, i) => {
+        return (
+            <GalleryItem key={i} item={item} />
+        )
+    })
+
     return (
         <div>
-            <GalleryItem />
+            {dataDisplay}
         </div>
     )
 }
