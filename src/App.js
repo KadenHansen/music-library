cb1e018831157bd8ff18c2b9c102fe52a8ca93ab
Kaden Hansen <kadendeanhansen@gmail.com>
2022-03-30 20:12:00 -0400

    fix search input and add styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { useEffect, useRef, useState } from "react"
+import { Fragment, useEffect, useRef, useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { DataContext } from "./context/DataContext"
 import { SearchContext } from "./context/SearchContext"
 import Gallery from "./components/Gallery"
 import SearchBar from "./components/SearchBar"
+import AlbumView from "./components/AlbumView"
+import ArtistView from "./components/ArtistView"
 
 function App() {
   let [message, setMessage] = useState("Search for Music!")
   let [data, setData] = useState([])
   let searchInput = useRef("")
+
+  const API_URL = 'https://itunes.apple.com/search?term='
 
   useEffect(() => {
   })
@@ -16,7 +21,7 @@ function App() {
     e.preventDefault()
     const fetchData = async () => {
       document.title = `${term} music`
-      const res = await fetch(`https://itunes.apple.com/search?term=the%20grateful%20dead`)
+      const res = await fetch(API_URL + term)
       const resData = await res.json()
       if ( resData.results.length > 0 ) {
         setData(resData.results)
@@ -29,16 +34,28 @@ function App() {
 
   return (
     <div className="App">
-      <SearchContext.Provider value={{
-        term: searchInput,
-        handleSearch: handleSearch
-      }}>
-        <SearchBar />
-      </SearchContext.Provider>
       {message}
-      <DataContext.Provider value={data}>
-        <Gallery />
-      </DataContext.Provider>
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <Fragment>
+              <SearchContext.Provider value={{
+                term: searchInput,
+                handleSearch: handleSearch
+              }}>
+                <SearchBar />
+              </SearchContext.Provider>
+              <DataContext.Provider value={data}>
+                <Gallery />
+              </DataContext.Provider>
+            </Fragment>
+          } />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
+      
+      
     </div>
   )
 }
---
+++ src/components/AlbumView.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export default function AlbumView() {
+    const [ albumData, setAlbumData ] = useState([])
+    const { id } = useParams()
+
+    return (
+        <div>
+            <h2>The id passed was: {id}</h2>
+            <p>Album View</p>
+        </div>
+    )
+}
